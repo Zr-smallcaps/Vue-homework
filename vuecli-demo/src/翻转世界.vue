94302774b344ec6db6c8f7fee8c255c4991d02db
Hayeszy <1282429235@qq.com>
2022-06-28 23:42:43 +0800

    first commit

---
+++ vuecli-demo/src/翻转世界.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button @click="btn">反转</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello world",
+    };
+  },
+  methods: {
+    btn() {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
