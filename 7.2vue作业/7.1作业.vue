410db49df7f36f322d8dd3072cb2da96efd79e34
George-George-George <100109574+George-George-George@users.noreply.github.com>
2022-07-02 17:18:11 +0800

    Add files via upload

---
+++ 7.2vue作业/7.1作业.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <span>国内网站：粉丝数量：{{ msg | fil(1) }}</span><br>
+        <span>国外网站：粉丝数量：{{ msg | fil(2) }}</span><br><br>
+        <span>时间:{{ timer | timeout(1) }}</span><br><br>
+        <span>时间:{{ timer | timeout(2) }}</span><br><br>
+
+    </div>
+</template>
+<script>
+import moment from "moment"
+export default {
+    data() {
+        return {
+            msg: '1241251251',
+            timer: 1636450540055
+        }
+    },
+    created() {
+    },
+    computed: {
+    },
+    methods: {
+    },
+    filters: {
+        fil(val, old) {
+            return old == 1 ? val / 1000 + '万' : val / 10000 + '千'
+        },
+        timeout(val, old) {
+            return old == 1 ? moment(val).format('YYYY-MM-DD') : moment(val).format('YYYY/MM/DD')
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ 7.2vue作业/淘宝购物车.vue
@@ -0,0 +1,151 @@
+<template>
+  <div id="app">
+    <table class="tb">
+      <tr>
+        <th><input type="checkbox" v-model="isAll" />全选</th>
+        <th>商品</th>
+        <th>单价</th>
+        <th>数量</th>
+        <th>小记</th>
+        <th>操作</th>
+      </tr>
+      <!-- 循环渲染的元素tr -->
+      <tr v-for="item in list" :key="item.id">
+        <td><input type="checkbox" v-model="item.flag" /></td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.price }}</td>
+        <td>
+          <span @click="subNum(item.id)">-</span>
+          <button>{{ item.num }}</button>
+          <span @click="item.num++">+</span>
+        </td>
+        <td>{{ item.text }}</td>
+        <td><button @click="del(item.id)">删除</button></td>
+      </tr>
+      <tr v-if="list.length === 0">
+        <td colspan="4">没有数据咯~</td>
+      </tr>
+    </table>
+    <br />
+    <button @click="delCheck">删除选中商品</button>
+    <button @click="delEvery">清理购物车</button>
+    <br />
+    <div style="margin-top: 20px">
+      <h2>统计</h2>
+      <p>已经选中商品件数:{{ allCount }}</p>
+      <p>总价:{{ allPrice || 0 }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShop: [],
+      list: [
+        { id: 1, name: '奔驰', price: 100, num: 1, text: '没钱', flag: false },
+        { id: 2, name: '宝马', price: 200, num: 1, text: '没钱', flag: false },
+        { id: 3, name: '奥迪', price: 300, num: 1, text: '没钱', flag: false },
+      ],
+    };
+  },
+  methods: {
+    subNum(val) {
+      let index = this.list.findIndex((ele) => ele.id === val);
+      this.list[index].num--;
+
+      if (this.list[index].num == 0) {
+        return this.del(this.list[index].id);
+      }
+    },
+    del(val) {
+      let index = this.list.findIndex((ele) => ele.id === val);
+      // 删除按钮 - 得到索引, 删除数组里元素
+      this.list.splice(index, 1);
+    },
+    delCheck() {
+      this.list = this.list.filter((item) => {
+        return !item.flag
+      });
+    },
+    delEvery() {
+      this.list = [];
+    },
+  },
+  computed: {
+    isAll: {
+      get() {
+        return this.list.every((item) => {
+          return item.flag;
+        });
+      },
+      set(val) {
+        this.list.forEach((item) => {
+          item.flag = val;
+        });
+      },
+    },
+    allCount: {
+      get() {
+        let count = this.list.reduce((count, nowVal) => {
+          if (nowVal.flag) {
+            count = count + nowVal.num;
+          }
+          return count;
+        }, 0);
+        return count;
+      },
+    },
+    allPrice: {
+      get() {
+        let count = this.list.reduce((count, nowVal) => {
+          if (nowVal.flag) {
+            count = count + nowVal.num * nowVal.price;
+          }
+          return count;
+        }, 0);
+        return count;
+      },
+    },
+  },
+  /*   watch: {
+    allCount: {
+      immediate: true,
+      handler(nowVal) {
+        if(this.)
+      },
+    },
+  }, */
+};
+</script>
+
+<style>
+#app {
+  width: 600px;
+  margin: 10px auto;
+}
+
+.tb {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.tb th {
+  background-color: #0094ff;
+  color: white;
+}
+
+.tb td,
+.tb th {
+  padding: 5px;
+  border: 1px solid black;
+  text-align: center;
+}
+
+.add {
+  padding: 5px;
+  border: 1px solid black;
+  margin-bottom: 10px;
+}
+</style>
