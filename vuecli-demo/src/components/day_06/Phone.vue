51aaa57b0009efa92d0dcb57b4583e5ad43d25e6
Hayeszy <1282429235@qq.com>
2022-07-07 00:51:30 +0800

    表单注册

---
+++ vuecli-demo/src/components/day_06/Phone.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <p>手机号码</p>
+            <slot name="num"></slot>
+        </div>
+        <div>
+            <p>登录密码</p>
+            <slot name="pwd"></slot>
+        </div>
+        <div>
+            <p>邀请码</p>
+            <slot name="code"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
--- vuecli-demo/src/main.js
+++ vuecli-demo/src/main.js
@@ -1,16 +1,12 @@
 import Vue from "vue";
-// import 'element-plus/dist/index.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';   
 import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "./assets/styles/base.css";
-import "./assets/styles/index.css";
 
 Vue.config.productionTip = false;
-// axios.defaults.baseURL = 'http://www.liulongbin.top:3006'
-// Vue.prototype.$axios = axios
-import axios from "axios";
-axios.defaults.baseURL = "https://www.escook.cn";
-Vue.prototype.$axios = axios;
+Vue.use(ElementUI);
+
 new Vue({
-    render: (h) => h(App),
-}).$mount("#app");
+    el: '#app',
+    render: h => h(App)
+})
