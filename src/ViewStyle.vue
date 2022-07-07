8c516bf071baea13d534d6ebf6063a7be0ec81c8
mengyu0207 <104361435+mengyu0207@users.noreply.github.com>
2022-07-07 21:08:02 +0800

    homework7.7

---
+++ src/ViewStyle.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+     <button class="wh" @click="conName='MyPhone'">手机注册</button>
+     <button class="wh" @click="conName='MyEmail'">邮箱注册</button>
+    
+
+      <keep-alive>
+       <component :is="conName"></component>
+      </keep-alive>
+  </div>
+</template>
+
+<script>
+import MyEmail from './components/MyEmail.vue'
+import MyPhone from './components/MyPhone.vue'
+export default {
+     data() {
+        return {
+            conName:"MyPhone",
+             activeName: 'second'
+        
+        }
+     },
+     components:{
+        MyEmail,
+        MyPhone
+     },
+      methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
+    }
+}
+</script>
+
+<style>
+ .wh{
+   width: 50%;
+    height: 100px;
+ }
+ .ww{
+  width: 50%;
+ }
+</style>
