2835d0407778c26d4d655ff472a66a3a6fdcc271
Tree2022 <1356863983@qq.com>
2022-07-07 21:09:36 +0800

    作业77完成

---
+++ 作业/homework0707/src/components/v-nubmer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="qwa" v-number name="" id="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      qwa: '',
+    };
+  },
+  directives: {
+    number: {
+      inserted(el, binding, vnode) {
+        el.value = el.value.replace(/[^\d.]/g, '');
+        vnode.context[binding.expression] = el.value;
+        console.log(el.value);
+      },
+      update(el, binding, vnode) {
+        console.log(el.value);
+        console.log(binding);
+        console.log(vnode);
+        el.value = el.value.replace(/[^\d.]/g, '');
+        vnode.context[binding.expression] = el.value;
+      },
+    },
+  },
+  components: {},
+};
+</script>
+
+<style></style>
